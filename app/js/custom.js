8ebe9511bf5abad357106d6d1b2649764bd1fa37
Roman Kalashnikov <rk@MacBook-Pro-Roman.local>
2019-09-09 23:43:52 +0300

    Added about page

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,3 +1,11 @@
 $(function() {
-    $('.menu__right-arrow')
+    $('.topline__burger').on('click', function() {
+        $('.menu').css('width', '33.3333%');
+    });
+
+    $('.menu__right-arrow').on('click', function() {
+        $('.menu').css('width', '0px');
+    });
+
+    
 });
